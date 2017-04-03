cdca4da98da6447a03c78c3b3b36746f8f37877a
Anwesh43 <anweshthecool0@gmail.com>
2017-04-03 18:23:43 +0530

    adding questions

--- app/src/main/java/com/anwesome/ui/progressivecheckformdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/progressivecheckformdemo/MainActivity.java
@@ -10,19 +10,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    String questions[] = {"What is A?","What is B?","What is C?","What is D?","What is E?"};
     String optionsContainer[][] = {{"A-Option1","A-Option2","A-Option3"},{"B-Option1","B-Option2","B-Option3"},{"C-Option1","C-Option2","C-Option3"},{"D-Option1","D-Option2","D-Option3"},{"E-Option1","E-Option2","E-Option3"}};
     List<List<String>> optionsList = new ArrayList<>();
+    List<String> questionList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int i= 0;
         for(String[] options:optionsContainer) {
             List<String> optionList = new ArrayList<>();
             for(String option:options) {
                 optionList.add(option);
             }
             optionsList.add(optionList);
+            questionList.add(questions[i]);
+            i++;
         }
-        FormContainerView formContainerView = new FormContainerView(this,optionsList);
+        FormContainerView formContainerView = new FormContainerView(this,optionsList,questionList);
         setContentView(formContainerView);
     }
 }
